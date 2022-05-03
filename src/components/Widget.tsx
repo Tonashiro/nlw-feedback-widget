5d56d18823d0198f9d08c93359db673a30e189fc
Tonashiro <vinit.tonashiro@hotmail.com>
2022-05-03 15:13:00 -0300

    aula-01

---
+++ src/components/Widget.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from "@headlessui/react";
+
+export function Widget() {
+  return (
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Teste</Popover.Panel>
+      <Popover.Button className=" bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6 " />
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+}
