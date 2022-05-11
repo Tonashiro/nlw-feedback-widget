300e16ca202e09f0d3dde6d6b80f27c6ed10609b
Tonashiro <vinit.tonashiro@hotmail.com>
2022-05-11 02:23:03 -0300

    next-level: add switch button for dark/light theme

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "phosphor-react";
 import { useEffect, useState } from "react";
+import Switch from "react-switch";
 
 export function Navbar() {
   const [isDarkMode, setIsDarkMode] = useState(
@@ -24,16 +25,23 @@ export function Navbar() {
 
   return (
     <nav className="bg-zinc-300 dark:bg-darkMode-bg h-[72px] w-full fixed z-10 px-6 py-5 flex items-center justify-end shadow-sm shadow-zinc-400 rounded-br-[40px] rounded-bl-[40px]">
-      <button
-        onClick={toggleDarkMode}
-        className="h-12 w-12 rounded-full bg-zinc-500 hover:bg-zinc-600 dark:bg-darkMode-component dark:hover:bg-zinc-500 transition-colors flex items-center justify-center"
-      >
-        {isDarkMode ? (
-          <Sun weight="bold" className="w-5 h-5 animate-pulse" />
-        ) : (
-          <Moon weight="bold" className="w-5 h-5 animate-pulse" />
-        )}
-      </button>
+      <Switch
+        onChange={toggleDarkMode}
+        checked={isDarkMode}
+        offColor="#27272A"
+        onColor="#F4F4F5"
+        onHandleColor="#27272A"
+        checkedIcon={
+          <Sun
+            weight="bold"
+            className="w-7 h-7 animate-pulse pl-2"
+            color="#27272A"
+          />
+        }
+        uncheckedIcon={
+          <Moon weight="bold" className="w-7 h-7 animate-pulse pl-1" />
+        }
+      />
     </nav>
   );
 }
